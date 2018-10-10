2063e95ad0c9cd14908e5e0aae8450858a9a2b5c
Randen Neil Rosete <randenneil.rosete@grabtaxi.com>
2018-10-10 20:19:06 +0800

    correct mongodb credentials

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://test_user:1test_user.com@ds666666.mlab.com:39999/div0rs
+export MONGODB_CREDENTIALS=mongodb://test_user:1test_user.com@ds227373.mlab.com:27373/div0rs
 
 # finally, run the app in dev mode...
 npm run dev
